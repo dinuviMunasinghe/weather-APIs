e4ba83aa0d7a96ba249ed93302484ab4a029d402
dinuviMunasinghe <dinuvi.upethra@gmail.com>
2021-07-18 14:57:48 +0530

    loading the data fetched from the json to the array

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,9 +1,32 @@
 import './App.css';
+import { cities } from "./cities";
+
+var list = [];
+
+function startUp() {
+  list = [];
+  console.log(cities.length)
+  for (let city of cities) {
+    console.log(city)
+    list.push(city);
+  }
+} 
 
 function App() {
+  const listItems = list.map((city) =>
+    <div className="cityDetails">
+      <p>{"id - " + city.CityCode + "  city name - " + city.CityName}</p>
+    </div>
+  );
+
   return (
     <div className="App">
-      <p>hello</p>
+      {startUp()}
+
+      <div className="city-data">
+        <p>Cities and their id's</p>
+        {listItems}
+      </div>
     </div>
   );
 }
